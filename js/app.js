d0351671df70eff518f51aa3b11a73d45dfb268f
jozefmarton <marton.jozef6@gmail.com>
2018-04-23 19:02:02 +0200

    contact-form, gallery

--- js/app.js
+++ js/app.js
@@ -13,3 +13,19 @@ function closeNav() {
 // https://github.com/michalsnik/aos
 AOS.init();
 
+// -------------------------------------SCROLL FUNCTIONS--------------------------------
+
+window.onscroll = function () {
+    myFunction()
+};
+
+var ele = document.body;
+
+function myFunction() {
+    if (window.pageYOffset > 100) {
+        ele.classList.add("sticky-nav")
+    } else {
+        ele.classList.remove("sticky-nav");
+    }
+}
+
